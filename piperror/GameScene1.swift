6747b86ac41c57bb353944ce93402391eed07a0f
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2021-04-09 13:29:04 +0300

    update lvl1

--- piperror/GameScene1.swift
+++ piperror/GameScene1.swift
@@ -11,14 +11,23 @@ import GameplayKit
 
 class GameScene1 : SKScene {
     
-    let shape = SKSpriteNode(color: .white, size: CGSize(width: 100, height: 100))
     
+    
+    let wall1 = SKSpriteNode(color: .white, size: CGSize(width: 100, height: 100))
+    let wall2 = SKSpriteNode(color: .white, size: CGSize(width: 100, height: 100))
+    let ball = SKSpriteNode(color: .white, size: CGSize(width: 100, height: 100))
     override func didMove(to view: SKView) {
         self.anchorPoint = .zero
-        shape.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        self.addChild(shape)
+        
+        wall1.size = CGSize(width: self.size.width / 5, height: self.size.height)
+        wall1.position = CGPoint(x: wall1.size.width / 2, y: self.size.height / 2)
+        wall2.size = CGSize(width: self.size.width / 5, height: self.size.height)
+        wall2.position = CGPoint(x: self.size.width - wall2.size.width / 2, y: self.size.height / 2)
+        ball.size = CGSize(width: self.size.width / 10, height: self.size.width / 10)
+        ball.position = CGPoint(x: self.size.width / 2, y: ball.size.height)
+        self.addChild(wall1)
+        self.addChild(wall2)
+        self.addChild(ball)
     }
-    
-    
-    
+
 }
