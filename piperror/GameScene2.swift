fa809c9660b7631a10586e747d1f064f6eded713
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2021-05-13 19:16:55 +0300

    add GameScenePattern

--- piperror/GameScene2.swift
+++ piperror/GameScene2.swift
@@ -9,16 +9,54 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene2 : SKScene {
+
+
+class GameScene2 : GameScenePattern {
     
-    let shape = SKSpriteNode(color: .green, size: CGSize(width: 100, height: 100))
+    var barrier1:BarrageNode!
+    var barrier2:BarrageNode!
+    var barrier3:BarrageNode!
+    var barrier4:BarrageNode!
     
     override func didMove(to view: SKView) {
-        self.anchorPoint = .zero
-        shape.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        self.addChild(shape)
+        super.didMove(to: view)
+        
+        let barrierRotate = SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 4)
+        let foreverBarrierRotate = SKAction.repeatForever(barrierRotate)
+        
+        barrier1 = BarrageNode(imageName: "barrier",
+                               size: CGSize(width: 0.03 * self.size.width, height: 0.225 * self.size.height),
+                               barrageCategory: barrierCategory,
+                               ballCategory: ballCategory)
+        barrier1.position = CGPoint(x: 0.64 * self.size.width, y: 0.5 * self.size.height)
+        barrier1.run(foreverBarrierRotate)
+        moving.addChild(barrier1)
+        
+        barrier2 = BarrageNode(imageName: "barrier2",
+                               size: CGSize(width: 0.22 * self.size.width, height: 0.05 * self.size.height),
+                               barrageCategory: barrierCategory,
+                               ballCategory: ballCategory)
+        barrier2.position = CGPoint(x: self.size.width - barrier1.position.x - barrier1.size.width / 2 + barrier2.size.width / 2,
+                                    y: 0.37 * self.size.height)
+        barrier2.run(foreverBarrierRotate)
+        moving.addChild(barrier2)
+        
+        barrier3 = BarrageNode(imageName: "barrier3",
+                               size: CGSize(width: 0.26 * self.size.width, height: 0.26 * self.size.width),
+                               barrageCategory: barrierCategory,
+                               ballCategory: ballCategory)
+        barrier3.position = CGPoint(x: barrier2.position.x - barrier2.size.width / 2 + barrier1.size.width / 2,
+                                    y: 0.60 * self.size.height)
+        barrier3.run(foreverBarrierRotate)
+        moving.addChild(barrier3)
+        barrier4 = BarrageNode(imageName: "barrier4",
+                               size: CGSize(width: barrier3.size.width, height: barrier3.size.width / 2),
+                               barrageCategory: barrierCategory,
+                               ballCategory: ballCategory)
+        barrier4.position = CGPoint(x: barrier1.position.x,
+                                    y: 0.70 * self.size.height)
+        barrier4.run(foreverBarrierRotate)
+        moving.addChild(barrier4)
     }
-    
-    
-    
+
 }
---
+++ piperror/GameScenePattern.swift
@@ -0,0 +1,117 @@
+//
+//  GameScenePattern.swift
+//  piperror
+//
+//  Created by Панышев Александр Сергеевич on 13.05.2021.
+//  Copyright © 2021 Панышев Александр Сергеевич. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScenePattern : SKScene, SKPhysicsContactDelegate {
+    
+    var ball:SKSpriteNode!
+    var moving:SKNode!
+    var gameover = Bool()
+    var gameStarted = Bool()
+    var startButton:ButtonNode!
+    var exitButton:ButtonNode!
+    let ballCategory: UInt32 = 1 << 0
+    let WallCategory: UInt32 = 1 << 1
+    let barrierCategory: UInt32 = 1 << 2
+
+
+    override func didMove(to view: SKView) {
+        gameover = false
+        gameStarted = false
+        
+        
+
+        
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = .zero
+        self.anchorPoint = .zero
+        self.backgroundColor = UIColor(#colorLiteral(red: 0.2177612185, green: 0.2177672982, blue: 0.2177640498, alpha: 1))
+        moving = SKNode()
+        moving.speed = 0
+        self.addChild(moving)
+        
+        //create start button
+        startButton = ButtonNode(imageNamed: "startbutton")
+        startButton.size = CGSize(width: 0.12 * self.size.width, height: 0.12 * self.size.width)
+        startButton.position = CGPoint(x: 0.1 * self.size.width, y: 0.1 * self.size.width)
+        startButton.handler = startButtonAction
+        self.addChild(startButton)
+        
+        //create exit button
+        exitButton = ButtonNode(imageNamed: "exitbutton")
+        exitButton.size = CGSize(width: 0.12 * self.size.width, height: 0.12 * self.size.width)
+        exitButton.position = CGPoint(x: 0.9 * self.size.width, y: 0.1 * self.size.width)
+        exitButton.handler = exitButtonAction
+        self.addChild(exitButton)
+        
+        //setup ball
+        let ballTexture = SKTexture(imageNamed: "ball.png")
+        ballTexture.filteringMode = .nearest
+        
+        ball = SKSpriteNode(texture: ballTexture)
+        ball.size = CGSize(width: 0.033 * self.size.width, height: 0.033 * self.size.width)
+        ball.position = CGPoint(x: self.size.width / 2, y: 0.28 * self.size.height)
+        
+        let ballrotate = SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)
+        let foreverballrotate = SKAction.repeatForever(ballrotate)
+        ball.run(foreverballrotate)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.height / 2.0)
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.allowsRotation = false
+        
+        ball.physicsBody?.categoryBitMask = ballCategory
+        ball.physicsBody?.collisionBitMask = WallCategory | barrierCategory
+        ball.physicsBody?.contactTestBitMask = WallCategory | barrierCategory
+        ball.speed = 0
+        self.addChild(ball)
+        
+        
+    }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        gameover = true
+        ball.speed = 0
+        moving.speed = 0
+        ball.physicsBody?.velocity = .zero
+    }
+    
+    func startButtonAction() {
+        if (!gameStarted){
+            gameStarted = true
+            moving.speed = 1
+            ball.speed = 1
+            ball.physicsBody?.velocity = CGVector(dx: 0, dy: 100)
+        }
+    }
+    
+    func exitButtonAction() {
+        let LevelMenu = LevelMenuScene(size: self.frame.size)
+        self.view?.presentScene(LevelMenu)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let touchPosition = touch!.location(in: self)
+        let touchNode = self.nodes(at: touchPosition)
+        
+        if (!gameStarted){
+            if touchNode.contains(startButton) {
+                gameStarted = true
+                moving.speed = 1
+                ball.speed = 1
+                ball.physicsBody?.velocity = CGVector(dx: 0, dy: 100)
+            }
+        }
+        else if (gameover){
+            let GameOverScene = StartMenuScene(size: self.frame.size)
+            self.view?.presentScene(GameOverScene)
+        }
+    }
+}
