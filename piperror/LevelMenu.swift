aeadec3fe6382d216ce595b0dee17aeeeca9eaf5
wayserlo <75145260+wayserlo@users.noreply.github.com>
2021-05-21 02:39:17 +0300

    change StartMenu, LevelMenu, Delete GameScene3

--- piperror/LevelMenu.swift
+++ piperror/LevelMenu.swift
@@ -17,126 +17,75 @@ class LevelMenuScene: SKScene {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
     var current_level = -1
+
+    var playButton : ButtonNode!
+    var exitButton : ButtonNode!
+    var level1 : ButtonNode!
+    var level2 : ButtonNode!
     
-    var playButton = SKSpriteNode()
-    var playLabel = SKLabelNode()
-    
-    var levels : [SKSpriteNode] = []
-    var levels_labels : [SKLabelNode] = []
-    
-    private func setLevelsButtons() {
-        
-        let delta_x = (self.size.width - 240)/4
-        
-        let level1 = SKSpriteNode(color: .white, size: CGSize(width: 80, height: 80))
-        level1.name = "level1"
-        level1.position = CGPoint(x: 40 + delta_x, y: self.size.height - (40 + delta_x))
-        
-        let level1label = SKLabelNode(fontNamed: "AvenirNext-Bold")
-        level1label.text = "1"
-        level1label.fontColor = .black
-        level1label.fontSize = CGFloat(40.0)
-        level1label.verticalAlignmentMode = .center
-        level1label.horizontalAlignmentMode = .center
-        
-        level1.addChild(level1label)
-        self.addChild(level1)
-        
-        levels.append(level1)
-        
-        let level2 = SKSpriteNode(color: .white, size: CGSize(width: 80, height: 80))
-        level2.name = "level2"
-        level2.position = CGPoint(x: 40*3 + delta_x*2, y: self.size.height - (40 + delta_x))
-        
-        let level2label = SKLabelNode(fontNamed: "AvenirNext-Bold")
-        level2label.text = "2"
-        level2label.fontColor = .black
-        level2label.fontSize = CGFloat(40.0)
-        level2label.verticalAlignmentMode = .center
-        level2label.horizontalAlignmentMode = .center
-        
-        
-        level2.addChild(level2label)
-        self.addChild(level2)
-        
-        levels.append(level2)
-        
-        
-        let level3 = SKSpriteNode(color: .white, size: CGSize(width: 80, height: 80))
-        level3.name = "level3"
-        level3.position = CGPoint(x: 40*5 + delta_x*3, y: self.size.height - (40 + delta_x))
-        
-        let level3label = SKLabelNode(fontNamed: "AvenirNext-Bold")
-        level3label.text = "3"
-        level3label.fontColor = .black
-        level3label.fontSize = CGFloat(40.0)
-        level3label.verticalAlignmentMode = .center
-        level3label.horizontalAlignmentMode = .center
-        
-        
-        level3.addChild(level3label)
-        self.addChild(level3)
-        
-        levels.append(level3)
+
+    func exitButtonAction() {
+    let StartMenu = StartMenuScene(size: self.frame.size)
+    self.view?.presentScene(StartMenu)
     }
     
-    override func didMove(to view: SKView) {
-        setLevelsButtons()
-        self.backgroundColor = .black
-        
-        let delta_x = (self.size.width - 240)/4
-        
-        playButton.name = "playButton"
-        playButton.size = CGSize(width: self.size.width * 0.75, height: self.size.height * 0.1)
-        playButton.color = .white
-        playButton.position = CGPoint(x: self.size.width / 2, y: delta_x + playButton.size.height/2)
-        
-        
-        playLabel.text = "Play"
-        playLabel.fontColor = .black
-        playLabel.fontSize = CGFloat(40.0)
-        playLabel.verticalAlignmentMode = .center
-        playLabel.horizontalAlignmentMode = .center
-        playLabel.fontName = "AvenirNext-Bold"
-        
-        
-        playButton.addChild(playLabel)
-        self.addChild(playButton)
+    func level1Action() {
+        current_level = 1
+        level2.texture = SKTexture(imageNamed: "level2_0")
+        level1.texture = SKTexture(imageNamed: "level1_1")
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first
-        let touchPosition = touch!.location(in: self)
-        let touchNode = self.nodes(at: touchPosition)
-        
-        
-        if touchNode.contains(playButton) && current_level != -1 {
-            if current_level == 1 {
-                let game_level = GameScene1(size: self.frame.size)
-                self.view?.presentScene(game_level)
-            } else if current_level == 2 {
-                    let game_level = GameScene2(size: self.frame.size)
-                    self.view?.presentScene(game_level)
-            } else if current_level == 3 {
-                let game_level = GameScene3(size: self.frame.size)
+    func level2Action() {
+        current_level = 2
+        level2.texture = SKTexture(imageNamed: "level2_1")
+        level1.texture = SKTexture(imageNamed: "level1_0")
+    }
+    
+    func playButtonAction() {
+        if current_level == 1 {
+            let game_level = GameScene1(size: self.frame.size)
+            self.view?.presentScene(game_level)
+        } else if current_level == 2 {
+                let game_level = GameScene2(size: self.frame.size)
                 self.view?.presentScene(game_level)
         } else {
-            
-        }
-        }
-
-        for (i, button) in levels.enumerated() {
-            if touchNode.contains(button) {
-                if current_level != -1 {
-                    levels[current_level - 1].color = .white
-                }
-                current_level = i + 1
-                button.color = .gray
-                break
-            }
+        
         }
     }
-}
+    
+    
+    override func didMove(to view: SKView) {
 
+    self.backgroundColor = UIColor(#colorLiteral(red: 0.2177612185, green: 0.2177672982, blue: 0.2177640498, alpha: 1))
+    
+    playButton = ButtonNode(imageNamed: "startbutton")
+    playButton.size = CGSize(width: 0.3 * self.size.width, height: 0.3 * self.size.width)
+    playButton.position = CGPoint(x: 0.7 * self.size.width, y: 0.4 * self.size.width)
+    playButton.texture = SKTexture(imageNamed: "startbutton")
+    playButton.handler = playButtonAction
+    self.addChild(playButton)
+
+    exitButton = ButtonNode(imageNamed: "exitbutton")
+    exitButton.size = CGSize(width: 0.3 * self.size.width, height: 0.3 * self.size.width)
+    exitButton.position = CGPoint(x: 0.3 * self.size.width, y: 0.4 * self.size.width)
+    exitButton.texture = SKTexture(imageNamed: "exitbutton")
+    exitButton.handler = exitButtonAction
+    self.addChild(exitButton)
+    
+    level1 = ButtonNode(imageNamed: "level1_0")
+    level1.size = CGSize(width: 0.3 * self.size.width, height: 0.3 * self.size.width)
+    level1.position = CGPoint(x: 0.25 * self.size.width, y: 1 * self.size.width)
+    level1.texture = SKTexture(imageNamed: "level1_0")
+    level1.handler = level1Action
+    self.addChild(level1)
+        
+    level2 = ButtonNode(imageNamed: "level2_0")
+    level2.size = CGSize(width: 0.3 * self.size.width, height: 0.3 * self.size.width)
+    level2.position = CGPoint(x: 0.75 * self.size.width, y: 1 * self.size.width)
+    level2.texture = SKTexture(imageNamed: "level2_0")
+    level2.handler = level2Action
+    self.addChild(level2)
+    
+    }
+}
